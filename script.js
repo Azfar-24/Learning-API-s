cb89841a7ead85cd8badfee8aa79afa20a6e5785
Azfar-24 <azfark87@gmail.com>
2022-03-11 19:15:16 +0530

    Completed but search is not working and Hover is not proper

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 const URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=124b078b3a77d5719ea1f5da1969d540"
 const imgPath = 'https://image.tmdb.org/t/p/w500'
-const searchURL = "https://api.themoviedb.org/3/search/movie?&api_key=124b078b3a77d5719ea1f5da1969d540&q='"
+const searchURL = "https://api.themoviedb.org/3/search/movie?&api_key=124b078b3a77d5719ea1f5da1969d540&query='"
 
 // API Key - 124b078b3a77d5719ea1f5da1969d540
 document.querySelector('main').innerHTML=''
@@ -10,13 +10,13 @@ const res = async function (API) {
   const data1 = await fetch(API);
   const data = await data1.json();
   const resources = data.results;
-  console.log(resources[0]);
+  console.log(resources);
   resources.forEach(element => {
     const html = `<div class="movie">
-    <img src=${element.poster_path} alt="" />
+    <img src=${imgPath + element.poster_path} alt="${element.original_title}" />
     <div class="name">
       <h3 class="heading">${element.original_title}</h3>
-      <span class="green">${element.vote_average}</span>
+      <span class="${getClass(element.vote_average)}">${element.vote_average}</span>
     </div>
     <div class="review">
      ${element.overview}
@@ -26,13 +26,24 @@ const res = async function (API) {
   });
 };
 res(URL);
+
+const getClass = function(vote){
+  if (vote>=8){
+    return 'green'}
+ else if (vote>5 && vote<8) {
+ return 'orange'
+} else {
+  return 'red'
+}
+}
+
 const form = document.querySelector('#form');
 const search = document.querySelector('#search');
 form.addEventListener('submit',(e)=>{
   e.preventDefault();
   const searchTerm = search.value;
 if (searchTerm && searchTerm === ''){
-  res(searchURL+searchTerm)
+  res(searchURL + searchTerm)
   search.value = ''
 }else {
   window.location.reload()
